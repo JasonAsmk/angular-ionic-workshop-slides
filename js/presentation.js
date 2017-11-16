45ca9850e244016ec108433356dfd5c9484bbeb4
jason <kaiseroskilo@gmail.com>
2017-11-16 18:39:24 +0200

    Write intro

---
+++ js/presentation.js
@@ -0,0 +1,13 @@
+// imports
+import odometer from 'odometer';
+import * as Rev from './reveal.js';
+
+
+(function() {
+
+  document.addEventListener('slidechanged', function(event) {
+  });
+
+
+ }
+)();
